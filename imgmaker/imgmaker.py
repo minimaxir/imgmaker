fdc6066bc2d5cfe28e20ae952ba748d1192c2715
Max Woolf <max@minimaxir.com>
2020-04-04 10:52:14 -0700

    Image Downsampling

--- imgmaker/imgmaker.py
+++ imgmaker/imgmaker.py
@@ -1,7 +1,10 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from PIL import Image
+import io
 
 scale = 2
+output_file = "test2x.png"
 
 chrome_options = Options()
 
@@ -17,4 +20,13 @@ driver = webdriver.Chrome(
     executable_path="/Users/maxwoolf/Downloads/chromedriver", options=chrome_options
 )
 driver.get("https://www.google.com")
-driver.get_screenshot_as_file("test.png")
+
+
+if scale > 1:
+    img = Image.open(io.BytesIO(driver.get_screenshot_as_png()))
+    img = img.resize(
+        (int(img.size[0] / scale), int(img.size[1] / scale)), Image.ANTIALIAS
+    )
+    img.save(output_file)
+else:
+    driver.get_screenshot_as_file(output_file)
